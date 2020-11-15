75d3e560574a0569421d20036c9214eba0b73c36
Muhammad Numair <numair.cs@gmail.com>
2020-11-16 04:54:44 +0500

    Binding Form With Previous Data

--- Sample Razor Pages Application/Pages/Admin/AddEditRecipe.cshtml.cs
+++ Sample Razor Pages Application/Pages/Admin/AddEditRecipe.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sample_Razor_Pages_Application.Models;
 
 namespace Sample_Razor_Pages_Application.Pages.Admin
 {
     public class AddEditRecipeModel : PageModel
     {
+        private IRecipesService recipesService;
+
         [FromRoute]
         public long? id { get; set; }
 
@@ -17,8 +20,14 @@ namespace Sample_Razor_Pages_Application.Pages.Admin
         }
 
         public Models.Recipe recipe { get; set; }
+
+        public AddEditRecipeModel(IRecipesService recipesService)
+        {
+            this.recipesService = recipesService;
+        }
         public void OnGet()
         {
+            recipe = recipesService.Find(id.GetValueOrDefault()) ?? new Recipe();
         }
     }
 }
