8894f79a1416910a82f849352181b5d7774a4b6f
Muhammad Numair <numair.cs@gmail.com>
2020-11-16 02:37:27 +0500

    Recipe Model & Index Page

---
+++ Sample Razor Pages Application/Pages/index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sample_Razor_Pages_Application.Pages
+{
+    public class indeModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
