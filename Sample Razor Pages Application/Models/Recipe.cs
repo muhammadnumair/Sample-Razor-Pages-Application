2ea05084d0e9b9c84517d54c44fe396dbca71823
Muhammad Numair <numair.cs@gmail.com>
2020-11-16 03:01:23 +0500

    Recipe Model Class Modification

--- Sample Razor Pages Application/Models/Recipe.cs
+++ Sample Razor Pages Application/Models/Recipe.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using static System.Environment;
 
 namespace Sample_Razor_Pages_Application.Models
 {
@@ -12,5 +14,46 @@ namespace Sample_Razor_Pages_Application.Models
         public string Description { get; set; }
         public string Directions { get; set; }
         public string Ingredients { get; set; }
+
+        public IEnumerable<string> DirectionsList
+        {
+            get { return (Directions ?? string.Empty).Split(NewLine); }
+        }
+
+        public IEnumerable<string> IngredientsList
+        {
+            get { return (Ingredients ?? string.Empty).Split(NewLine); }
+        }
+
+        #region Image
+
+        public byte[] Image { get; set; }
+
+        public string ImageContentType { get; set; }
+
+        public string GetInlineImageSrc()
+        {
+            if (Image == null || ImageContentType == null)
+                return null;
+
+            var base64Image = System.Convert.ToBase64String(Image);
+            return $"data:{ImageContentType};base64,{base64Image}";
+        }
+
+        public void SetImage(Microsoft.AspNetCore.Http.IFormFile file)
+        {
+            if (file == null)
+                return;
+
+            ImageContentType = file.ContentType;
+
+            using (var stream = new System.IO.MemoryStream())
+            {
+                file.CopyTo(stream);
+                Image = stream.ToArray();
+            }
+        }
+
+        #endregion
     }
 }
