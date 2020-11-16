7ffa02b034477d106b281843725046ff654a75cc
Muhammad Numair <numair.cs@gmail.com>
2020-11-16 05:30:24 +0500

    Uploading Images & Form Data

--- Sample Razor Pages Application/Pages/Admin/AddEditRecipe.cshtml.cs
+++ Sample Razor Pages Application/Pages/Admin/AddEditRecipe.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Sample_Razor_Pages_Application.Models;
@@ -19,15 +20,48 @@ namespace Sample_Razor_Pages_Application.Pages.Admin
             get { return id == null; } 
         }
 
+        [BindProperty]
         public Models.Recipe recipe { get; set; }
 
+        [BindProperty]
+        public IFormFile Image { get; set; }
         public AddEditRecipeModel(IRecipesService recipesService)
         {
             this.recipesService = recipesService;
         }
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-            recipe = recipesService.Find(id.GetValueOrDefault()) ?? new Recipe();
+            recipe = await recipesService.FindAsync(id.GetValueOrDefault()) ?? new Recipe();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            recipe.Id = id.GetValueOrDefault();
+            var updatedRecipe = await recipesService.FindAsync(id.GetValueOrDefault()) ?? new Recipe();
+            updatedRecipe.Id = recipe.Id;
+            updatedRecipe.Name = recipe.Name;
+            updatedRecipe.Description = recipe.Description;
+            updatedRecipe.Ingredients = recipe.Ingredients;
+            updatedRecipe.Directions = recipe.Directions;
+
+            if(Image != null)
+            {
+                using ( var stream = new System.IO.MemoryStream())
+                {
+                    await Image.CopyToAsync(stream);
+                    updatedRecipe.Image = stream.ToArray();
+                    updatedRecipe.ImageContentType = Image.ContentType;
+                }
+            }
+
+            await recipesService.SaveAsync(updatedRecipe);
+            return RedirectToPage("/Recipe", new { id = recipe.Id });
+        }
+
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await recipesService.DeleteAsync(id.Value);
+            return RedirectToPage("/Index");
         }
     }
 }
